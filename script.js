727fbbf2426357bfb5c77329109dd147923fee1e
Ashley Tessitore <ashleytessitore@proton.me>
2026-03-12 09:54:55 -0400

    Add contact information

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let contact = document.getElementById("contact");
+let email = document.getElementById("email");
 
 contact.addEventListener("click", sayHello);
 
 function sayHello() {
-  alert("Under Construction - Thanks for visiting my profile!");
+    if (email.hidden) {
+        contact.textContent = "Hide!"
+    } else {
+        contact.textContent = "Say Hello!"
+    }
+    email.hidden = !email.hidden;
 }
